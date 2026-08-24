deb3b156bec3d371f581e94456ea3d076a81862c
Ritesh Raikwar <ritesh06raikwar@gmail.com>
2026-08-25 01:48:12 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (38.69%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
+    int missingNumber(vector<int>& nums){
+        // int n = nums.size();
+        // vector<bool>check(n+1,false);
+        // for(int i=0 ; i<n ;i++){
+        //     int ele = nums[i];
+        //     check[ele] = true;
+        // }
+        // for(int i=0 ; i<=n ; i++){
+        //     if(check[i]==false)  return i;
+        // }
+        // return 100;
         int n = nums.size();
-        vector<bool>check(n+1,false);
-        for(int i=0 ; i<n ;i++){
-            int ele = nums[i];
-            check[ele] = true;
+        int i = 0;
+        while(i<n){
+            int correctIdx= nums[i];
+            if(correctIdx==i || nums[i]==n) i++;
+            else swap(nums[i],nums[correctIdx]);
         }
-        for(int i=0 ; i<=n ; i++){
-            if(check[i]==false)  return i;
+        for(int i=0 ; i<n ; i++){
+            if(nums[i]!=i) return i;
         }
-        return 100;
+        return n;
     }
 };
