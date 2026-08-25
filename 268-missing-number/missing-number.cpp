34ac0a3a48c071d287048f27753f1c3e424c85b6
Ritesh Raikwar <ritesh06raikwar@gmail.com>
2026-08-26 02:06:19 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (94.01%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums){
+        //Method-1
         // int n = nums.size();
         // vector<bool>check(n+1,false);
         // for(int i=0 ; i<n ;i++){
@@ -11,16 +12,28 @@ public:
         //     if(check[i]==false)  return i;
         // }
         // return 100;
-        int n = nums.size();
-        int i = 0;
-        while(i<n){
-            int correctIdx= nums[i];
-            if(correctIdx==i || nums[i]==n) i++;
-            else swap(nums[i],nums[correctIdx]);
-        }
-        for(int i=0 ; i<n ; i++){
-            if(nums[i]!=i) return i;
-        }
-        return n;
+
+        // Method-2  CYCLIC SORT
+    //     int n = nums.size();
+    //     int i = 0;
+    //     while(i<n){
+    //         int correctIdx= nums[i];
+    //         if(correctIdx==i || nums[i]==n) i++;
+    //         else swap(nums[i],nums[correctIdx]);
+    //     }
+    //     for(int i=0 ; i<n ; i++){
+    //         if(nums[i]!=i) return i;
+    //     }
+    //     return n;
+    // }
+
+    //Method-3 
+    int n = nums.size();
+    int actualsum = n*(n+1)/2;
+    int arraySum = 0;
+    for(int i=0 ; i<n ;i++){
+        arraySum+=nums[i];
+    }
+    return actualsum-arraySum;
     }
 };
