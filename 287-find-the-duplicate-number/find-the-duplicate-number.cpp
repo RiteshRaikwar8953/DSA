615136fc1e9432dbf0cf2df7476755b30113ba4b
Ritesh Raikwar <ritesh06raikwar@gmail.com>
2026-08-27 17:05:06 +0530

    Time: 3 ms (68.42%) | Memory: 65 MB (90.75%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -4,9 +4,8 @@ public:
         int n = nums.size();
         int i = 0;
         while(i<n){
-            int correctidx = nums[i] - 1;
-            if(i==correctidx) i++;
-            else if(nums[i]==nums[correctidx]) return nums[i];
+            int correctidx = nums[i];
+            if(nums[i]==nums[correctidx]) return nums[i];
             else swap(nums[i],nums[correctidx]);
         }
         return 0;
