24b5af3fbcf88ccdcfcebce03c69f386a2048867
Ritesh Raikwar <ritesh06raikwar@gmail.com>
2026-09-03 01:57:54 +0530

    Time: 3 ms (51.94%) | Memory: 55.2 MB (19.90%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -9,7 +9,7 @@ public:
             else swap(nums[i],nums[nums[i]-1]);
         }
         for(int i=0 ; i<n ; i++){
-            if(nums[i]!=i+1) return i+1; 
+            if(nums[i]!=(i+1)) return i+1; 
         }
         return n+1;
     }
