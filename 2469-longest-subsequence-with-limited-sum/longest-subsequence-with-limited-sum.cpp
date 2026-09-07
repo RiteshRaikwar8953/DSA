89beba3c7f88436f3abcb3a89881c3e28491002c
Ritesh Raikwar <ritesh06raikwar@gmail.com>
2026-09-08 01:02:49 +0530

    Time: 4 ms (51.02%) | Memory: 17.7 MB (96.43%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
@@ -5,19 +5,25 @@ public:
         int m = queries.size();
         vector<int>ans(m);
         //SORT nums
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());  //TC = 0(nlogn) , SC = O(logn)
         //Make Nums prefix Sum
         for(int i=1 ; i<n ;i++){
             nums[i]+=nums[i-1];
         }
-        for(int i=0 ; i<m ; i++){
-            int length = 0;
-            for(int j=0 ; j<n ;j++){
-                if(nums[j]>queries[i])  break;
-                length++;
+        for(int i=0 ; i<m ; i++){  //TC = O(m*n)
+            int maxlen = 0;
+            int lo = 0; //Binary Search
+            int hi = n-1;
+            while(lo<=hi){
+                int mid = lo+(hi-lo)/2;
+                if(nums[mid]>queries[i])  hi = mid-1;
+                else{
+                    maxlen = mid+1;
+                    lo = mid+1;
+                }
             }
-            ans[i] = (length);
+            ans[i] = (maxlen);
         }
-        return ans;
+        return ans;  //TC = O(n*(m+logn)) ~ O(n*m)
     }
 };
