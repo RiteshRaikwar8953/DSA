811c2ca82570427834cf36fc48d89b32b6a5fc9d
Ritesh Raikwar <ritesh06raikwar@gmail.com>
2026-09-18 23:51:42 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (60.51%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    vector<int> plusOne(vector<int>& digits) {
-        for (int i = digits.size() - 1; i >= 0; i--) {
+    vector<int> plusOne(vector<int>& digits){
+        int n = digits.size();
+        for (int i = n - 1; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i]++;
                 return digits;
