b62b0f6d1c03636c99e0c080e5aa017598c0036a
Ritesh Raikwar <ritesh06raikwar@gmail.com>
2026-09-21 21:15:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (67.84%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -3,8 +3,7 @@ public:
     int lengthOfLastWord(string s) {
         int n = s.size();
         int length = 0;
-        int j=n-1;
-        int k=n-1;
+        int j=n-1 , k=n-1;
         while(s[k]==' '){
             j--;
             k--;
