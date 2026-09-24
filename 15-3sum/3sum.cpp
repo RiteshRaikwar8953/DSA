8f0b0c24c6a7e0c4abe6b3de695e906200da2b89
Ritesh Raikwar <ritesh06raikwar@gmail.com>
2026-09-24 23:08:52 +0530

    Time: 43 ms (82.63%) | Memory: 29.1 MB (45.61%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,8 +4,7 @@ public:
         vector<vector<int>> ans;
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < n - 2; i++) {
-            // Skip duplicate first values
+        for(int i = 0; i < n - 2; i++){
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
